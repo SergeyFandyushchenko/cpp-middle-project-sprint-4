18f94b55f4f99c58578e77e60bd604edcaf84130
SergeyFandyushchenko <fandyuschenko@yandex.ru>
2026-07-06 04:44:35 +0000

    Second

--- src/function.cpp
+++ src/function.cpp
@@ -11,6 +11,10 @@
 #include "file.hpp"
 #include "utils.hpp"
 
+namespace {
+constexpr int OFFSET_POS_SECOND = 2;  // Смещение позиции второго значения в интервале после ", "
+}
+
 namespace analyzer::function {
 
 std::vector<Function> FunctionExtractor::Get(const analyzer::file::File &file) {
@@ -60,7 +64,7 @@ FunctionExtractor::FunctionNameLocation FunctionExtractor::GetNameLocation(const
 
     size_t comma = coords.find(',');
     Position start{static_cast<size_t>(ToInt(coords.substr(0, comma))),
-                   static_cast<size_t>(ToInt(coords.substr(comma + 2)))};
+                   static_cast<size_t>(ToInt(coords.substr(comma + OFFSET_POS_SECOND)))};
 
     size_t dash = function_ast.find('[', coord_end);
     size_t end_bracket = function_ast.find(']', dash);
@@ -68,7 +72,7 @@ FunctionExtractor::FunctionNameLocation FunctionExtractor::GetNameLocation(const
 
     comma = end_coords.find(',');
     Position end{static_cast<size_t>(ToInt(end_coords.substr(0, comma))),
-                 static_cast<size_t>(ToInt(end_coords.substr(comma + 2)))};
+                 static_cast<size_t>(ToInt(end_coords.substr(comma + OFFSET_POS_SECOND)))};
 
     return {start, end, ""};
 }
@@ -99,7 +103,7 @@ FunctionExtractor::FindEnclosingClass(const std::string &ast, const FunctionName
 
         size_t comma = coords.find(',');
         Position class_start{static_cast<size_t>(ToInt(coords.substr(0, comma))),
-                             static_cast<size_t>(ToInt(coords.substr(comma + 2)))};
+                             static_cast<size_t>(ToInt(coords.substr(comma + OFFSET_POS_SECOND)))};
 
         size_t dash = ast.find('-', coord_end);
         size_t second_coord_start = ast.find('[', dash);
@@ -108,7 +112,7 @@ FunctionExtractor::FindEnclosingClass(const std::string &ast, const FunctionName
 
         comma = end_coords.find(',');
         Position class_end{static_cast<size_t>(ToInt(end_coords.substr(0, comma))),
-                           static_cast<size_t>(ToInt(end_coords.substr(comma + 2)))};
+                           static_cast<size_t>(ToInt(end_coords.substr(comma + OFFSET_POS_SECOND)))};
 
         if (func_loc.start.line > class_start.line ||
             (func_loc.start.line == class_start.line && func_loc.start.col >= class_start.col)) {
